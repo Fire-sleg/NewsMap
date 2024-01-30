0467f92731f3d2cb038d9be5e6024286341827b8
Fire-sleg <fireslegstars@gmail.com>
2024-01-30 15:44:33 +0200

    add service and call the web api from angular client

--- NewsMap.UI/src/app/app.component.ts
+++ NewsMap.UI/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NewsMapService } from './services/news-map.service';
+import { NewsMap } from './models/NewsMap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'NewsMap.UI';
+
+  constructor(private newsMapService: NewsMapService) {}
+
+  ngOnInit(): void {
+    this.newsMapService.getNewsMap().subscribe((result: NewsMap) => {
+      this.title = result.news as string;
+    });
+  }
 }
--- NewsMap.UI/src/environments/environment.development.ts
+++ NewsMap.UI/src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7208/api/',
+};
