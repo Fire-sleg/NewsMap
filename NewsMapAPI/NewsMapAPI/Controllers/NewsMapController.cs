e59bc00680f16d6b8000bf6de65f29ec98e96182
Fire-sleg <fireslegstars@gmail.com>
2024-02-03 19:26:45 +0200

    adding post request for region name

--- NewsMapAPI/NewsMapAPI/Controllers/NewsMapController.cs
+++ NewsMapAPI/NewsMapAPI/Controllers/NewsMapController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NewsMapAPI.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace NewsMapAPI.Controllers
 {
@@ -11,7 +12,13 @@ namespace NewsMapAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<NewsMap>> GetNewsMap()
         {
-            return new NewsMap() { News = "news" };
+            return Ok(new NewsMap() { News = "news", Region = "the best region" });
+        }
+        [HttpPost]
+        public async Task<ActionResult<NewsMap>> PostRegion([FromBody] NewsMap newsMap)
+        {
+            // Логіка обробки даних тут
+            return Ok(newsMap);
         }
 
     }
